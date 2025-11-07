ef638d33e32ab864e4a86748013fc26423ecb084
Nathaniel Obeng <nathanielobeng0@gmail.com>
2025-11-07 20:55:03 +0000

    Committee changed to simple resource

--- app/Filament/Resources/Committees/CommitteeResource.php
+++ app/Filament/Resources/Committees/CommitteeResource.php
@@ -2,55 +2,109 @@
 
 namespace App\Filament\Resources\Committees;
 
-use App\Filament\Resources\Committees\Pages\CreateCommittee;
-use App\Filament\Resources\Committees\Pages\EditCommittee;
-use App\Filament\Resources\Committees\Pages\ListCommittees;
-use App\Filament\Resources\Committees\Pages\ViewCommittee;
-use App\Filament\Resources\Committees\Schemas\CommitteeForm;
-use App\Filament\Resources\Committees\Schemas\CommitteeInfolist;
-use App\Filament\Resources\Committees\Tables\CommitteesTable;
+use App\Filament\Resources\Committees\Pages\ManageCommittees;
 use App\Models\Committee;
 use BackedEnum;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class CommitteeResource extends Resource
 {
-    protected static ?string $model = Committee::class;
+  protected static ?string $model = Committee::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+  protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserGroup;
 
-    public static function form(Schema $schema): Schema
-    {
-        return CommitteeForm::configure($schema);
-    }
+  protected static ?string $recordTitleAttribute = 'Committees Members';
 
-    public static function infolist(Schema $schema): Schema
-    {
-        return CommitteeInfolist::configure($schema);
-    }
+  public static function form(Schema $schema): Schema
+  {
+    return $schema
+      ->components([
+        Select::make('title')
+          ->options([
+            'Mr' => 'Mr',
+            'Mrs' => 'Mrs',
+            'Miss' => 'Miss',
+            'Dr' => 'Dr',
+            'Prof' => 'Prof',
+            'Eng' => 'Eng',
+            'Pln' => 'Pln',
+            'Esq' => 'Esq',
+          ])
+          ->required(),
+        TextInput::make('firstname')->required()->placeholder('Type in member\s firstname'),
+        TextInput::make('lastname')->required()->placeholder('Type in member\s lastname'),
+        // ])->columns(5),
+        Select::make('panel')
+          ->options([
+            'SPC' => 'SPC',
+            'TSC' => 'TSC',
+          ])
+          ->required(),
+        TextInput::make('email')->required()->email()->placeholder('Type in member\s email address'),
+        TextInput::make('designation')->required()->placeholder('Type in member\s designation'),
+        Select::make('role')
+          ->options([
+            'Chairperson' => 'Chairperson',
+            'Secretary' => 'Secretary',
+            'Member' => 'Member',
+            'Other' => 'Other',
+          ])
+          ->required(),
+        TextInput::make('contact')->label('Phone number')->required()->placeholder('Type in member\s phone number'),
+      ]);
+  }
 
-    public static function table(Table $table): Table
-    {
-        return CommitteesTable::configure($table);
-    }
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->recordTitleAttribute('Committees')
+      ->columns([
+        TextColumn::make('name')->label('Full name')
+          ->default(fn(Model $member) => "{$member->title} {$member->firstname} {$member->lastname}")
+          ->searchable(),
+        TextColumn::make('contact')->label('Phone number')->searchable(),
+        TextColumn::make('email')->label('Email')->searchable(),
+        TextColumn::make('panel')
+          ->badge()
+          ->color(fn(string $state) => match ($state) {
+            'TSC' => 'info',
+            'SPC' => 'primary',
+          })
+      ])
+      ->filters([
+        //
+      ])
+      ->recordActions([
+        ActionGroup::make([
+          ViewAction::make(),
+          EditAction::make(),
+          DeleteAction::make(),
+        ])
+      ])
+      ->toolbarActions([
+        BulkActionGroup::make([
+          DeleteBulkAction::make(),
+        ]),
+      ]);
+  }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
-
-    public static function getPages(): array
-    {
-        return [
-            'index' => ListCommittees::route('/'),
-            // 'create' => CreateCommittee::route('/create'),
-            'view' => ViewCommittee::route('/{record}'),
-            'edit' => EditCommittee::route('/{record}/edit'),
-        ];
-    }
+  public static function getPages(): array
+  {
+    return [
+      'index' => ManageCommittees::route('/'),
+    ];
+  }
 }
--- app/Filament/Resources/Extracts/ExtractResource.php
+++ app/Filament/Resources/Extracts/ExtractResource.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Extracts;
 
 use App\Filament\Resources\Extracts\Pages\ManageExtracts;
 use App\Models\Extract;
+use App\Models\Locality;
+use App\Models\Sector;
 use BackedEnum;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
@@ -13,72 +15,145 @@ use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ExtractResource extends Resource
 {
-    protected static ?string $model = Extract::class;
+  protected static ?string $model = Extract::class;
 
-    protected static string|BackedEnum|null $navigationIcon = 'icon-file-certificate';
+  protected static string|BackedEnum|null $navigationIcon = 'icon-file-certificate';
 
-    protected static ?string $recordTitleAttribute = 'Extracts';
+  protected static ?string $recordTitleAttribute = 'Extracts';
 
-    public static function form(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                TextInput::make('Extracts')
-                    ->required()
-                    ->maxLength(255),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->recordTitleAttribute('Extracts')
-            ->columns([
-                TextColumn::make('Extracts')
-                    ->searchable(),
+  public static function form(Schema $schema): Schema
+  {
+    return $schema
+      ->components([
+        Group::make([
+          Select::make('title')
+            ->options([
+              'Mr' => 'Mr',
+              'Mrs' => 'Mrs',
+              'Miss' => 'Miss',
+              'Dr' => 'Dr',
+              'Prof' => 'Prof',
+              'Eng' => 'Eng',
+              'Pln' => 'Pln',
+              'Esq' => 'Esq',
             ])
-            ->filters([
-                TrashedFilter::make(),
-            ])
-            ->recordActions([
-                EditAction::make(),
-                DeleteAction::make(),
-                ForceDeleteAction::make(),
-                RestoreAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                    ForceDeleteBulkAction::make(),
-                    RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
+            ->required(),
+          TextInput::make('firstname')->required()->placeholder('Type in member\'s firstname')->columnSpan(2),
+          TextInput::make('lastname')->required()->placeholder('Type in member\'s lastname')->columnSpan(2),
+        ])
+          ->columnSpanFull()
+          ->columns(5),
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => ManageExtracts::route('/'),
-        ];
-    }
 
-    public static function getRecordRouteBindingEloquentQuery(): Builder
-    {
-        return parent::getRecordRouteBindingEloquentQuery()
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
-    }
+        Group::make([
+          Select::make('locality_id')
+            ->options(Locality::all()->pluck('name', 'id'))
+            ->required()
+            ->columnSpan(2)
+            ->live(),
+
+          Select::make('sector_id')
+            ->options(function (Get $get) {
+              return Sector::where('locality_id', $get('locality_id'))->get()->pluck('name', 'id');
+            })
+            ->required()
+            ->columnSpan(2)
+            ->live(),
+
+          Select::make('block')
+            ->options(function (Get $get): array {
+              $sector = Sector::find($get('sector_id'));
+              logger('selected blocks', ['blocks' => $sector]);
+              return $sector->blocks ?? [];
+            })
+            ->required()
+            ->columnSpan(2),
+
+          TextInput::make('plot_number')->required()->placeholder('Type in plot no.'),
+        ])
+          ->columnSpanFull()
+          ->columns(7),
+
+
+        Group::make([
+          TextInput::make('phone_number')->label('Phone number')
+            ->rules(['min:10', 'max:13'])
+            ->required()
+            ->tel()
+            ->telRegex('/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/')
+            ->placeholder('Type in phone number'),
+
+          DatePicker::make('allocation_date')->required(),
+          DatePicker::make('registration_date')->required()
+        ])
+          ->columnSpanFull()
+          ->columns(3),
+      ]);
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->recordTitleAttribute('Extracts')
+      ->columns([
+        TextColumn::make('name')->label('Full name')
+          ->default(fn(Model $staff) => "{$staff->title} {$staff->firstname} {$staff->lastname}")
+          ->searchable(),
+        TextColumn::make('phone_number')->searchable(),
+        TextColumn::make('locality.name')->searchable(),
+        TextColumn::make('sector.name')->searchable(),
+        TextColumn::make('block')->searchable(),
+        TextColumn::make('plot_number')->searchable(),
+        TextColumn::make('allocation_date')->date(),
+        TextColumn::make('registration_date')->date(),
+      ])
+      ->filters([
+        TrashedFilter::make(),
+      ])
+      ->recordActions([
+        EditAction::make(),
+        DeleteAction::make(),
+        ForceDeleteAction::make(),
+        RestoreAction::make(),
+      ])
+      ->toolbarActions([
+        BulkActionGroup::make([
+          DeleteBulkAction::make(),
+          ForceDeleteBulkAction::make(),
+          RestoreBulkAction::make(),
+        ]),
+      ]);
+  }
+
+  public static function getPages(): array
+  {
+    return [
+      'index' => ManageExtracts::route('/'),
+    ];
+  }
+
+  public static function getRecordRouteBindingEloquentQuery(): Builder
+  {
+    return parent::getRecordRouteBindingEloquentQuery()
+      ->withoutGlobalScopes([
+        SoftDeletingScope::class,
+      ]);
+  }
 }
